36c5ec28fd7bc73b75e643fa685ecadc80d42f1d
Ponniah <k.ponniah@gmail.com>
2018-01-19 16:35:53 +0530

    first commit

---
+++ NetTest01/Assets/NetTest01.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 
+// Unity 2017.3 Manual Tutorials
+//
+
+
+public class NetTest01 : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
